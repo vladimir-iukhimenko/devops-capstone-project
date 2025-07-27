56b688ae05bf4ab267e5d68867a98b4b12fad369
Vladimir Iukhimenko <vlad.imenko@yandex.ru>
2025-07-27 07:13:12 -0400

    added other ops

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -137,3 +137,39 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)    
+    
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+
+        test_account = AccountFactory()
+        # send a self.client.post() request to the BASE_URL with a json payload of test_account.serialize()
+        # assert that the resp.status_code is status.HTTP_201_CREATED
+
+        # update the account
+
+        # get the data from resp.get_json() as new_account
+        # change new_account["name"] to something known
+        # send a self.client.put() request to the BASE_URL with a json payload of new_account
+        # assert that the resp.status_code is status.HTTP_200_OK
+        # get the data from resp.get_json() as updated_account
+        # assert that the updated_account["name"] is whatever you changed it to     
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        # send a self.client.delete() request to the BASE_URL with an id of an account
+        # assert that the resp.status_code is status.HTTP_204_NO_CONTENT   
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
