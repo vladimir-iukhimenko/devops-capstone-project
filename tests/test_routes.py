0a3512c27dd6e49e6a6a3e19160fdf77adfe718a
Vladimir Iukhimenko <vlad.imenko@yandex.ru>
2025-07-29 14:27:59 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -148,7 +148,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)    
-    
+
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -196,3 +196,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')    
+        
